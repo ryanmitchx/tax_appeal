58dd07a38ae98df0cf50a2cc6377eff3328168c5
Ryan Mitchell <rjmitche@usc.edu>
2020-01-31 21:49:24 -0800

    initial commit x2

---
+++ TaxAppeal/TaxAppeal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxAppeal
+//
+//  Created by Ryan on 31/01/2020.
+//  Copyright © 2020 BRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
